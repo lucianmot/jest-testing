959c744b8de2957a6c59b15dfccec2a6771b3949
lucian <37814580+lucianmot@users.noreply.github.com>
2019-10-20 23:41:26 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const AppDisplay =  ()=>(
     <div>
         <div>
             <Link to={`/`}>
-                <h1>Isomorphic React Slow and Steady wins the Race</h1>
+                <h1>Isomorphic React Slow</h1>
             </Link>
         </div>
         <div>
