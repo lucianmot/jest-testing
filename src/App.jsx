abc73b02e8d0782fa457f0d22fa1f75a9dde5dc4
lucian <37814580+lucianmot@users.noreply.github.com>
2019-10-23 06:08:52 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const AppDisplay =  ()=>(
     <div>
         <div>
             <Link to={`/`}>
-                <h1>Isomorphic React 123</h1>
+                <h1>Isomorphic React not real</h1>
             </Link>
         </div>
         <div>
