f942a98ee7de34908901cfc83c3c4175ad25431c
lucian <37814580+lucianmot@users.noreply.github.com>
2019-10-23 06:09:26 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const AppDisplay =  ()=>(
     <div>
         <div>
             <Link to={`/`}>
-                <h1>Isomorphic React working on the cv</h1>
+                <h1>Isomorphic React working stop</h1>
             </Link>
         </div>
         <div>
