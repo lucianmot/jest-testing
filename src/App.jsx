39315dcc1df9235ea9d19e41d1b56bce3912eff0
lucian <37814580+lucianmot@users.noreply.github.com>
2019-10-30 15:03:53 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const AppDisplay =  ()=>(
     <div>
         <div>
             <Link to={`/`}>
-                <h1>Isomorphic React</h1>
+                <h1>Isomorphic React breaking</h1>
             </Link>
         </div>
         <div>
