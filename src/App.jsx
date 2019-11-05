d4781762b6f25f7636fd3a4f57c9aab08f2e8e35
lucian <37814580+lucianmot@users.noreply.github.com>
2019-11-05 17:32:52 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const AppDisplay =  ()=>(
     <div>
         <div>
             <Link to={`/`}>
-                <h1>Isomorphic React qwerty</h1>
+                <h1>Isomorphic React</h1>
             </Link>
         </div>
         <div>
